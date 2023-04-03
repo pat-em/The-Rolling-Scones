230e32e506b8b9375e0a87edb5c205f3507d908b
Patrycja Kopinska <maliszewska.p@gmail.com>
2023-04-03 12:20:51 +0200

    adding a list of commands

--- The_Rolling_Scones_bakery.py
+++ The_Rolling_Scones_bakery.py
@@ -8,10 +8,21 @@ while user_profile != "1" or user_profile != "2":
     print("Select 1 or 2:")
     user_profile = input()
     if user_profile == "1":
-        user_profile == "Bakery worker"
+        user_profile = "Bakery worker"
         print("Bakery worker profile")
         break
     if user_profile == "2":
-        user_profile == "Customer"
+        user_profile = "Customer"
         print("Customer profile")
         break
+
+if user_profile == "Bakery worker":
+    print("""Choose what you want to do:
+    1. Create a new bake
+    2. Change an existing bake
+    """)
+if user_profile == "Customer":
+    print("""Choose what you want to do:
+    1. Submit your order
+    2. Browse the offer
+    3. Browse favorites""")    
