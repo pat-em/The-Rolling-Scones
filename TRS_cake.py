0e3b2a1a9622e6d2caa226472d13f65c137f1295
Patrycja Kopinska <maliszewska.p@gmail.com>
2023-04-03 17:36:59 +0200

    adding def create_bake

--- TRS_cake.py
+++ TRS_cake.py
@@ -1,5 +1,6 @@
 class Cake:
     known_types = ['cake', 'muffin', 'meringue', 'biscuit', 'eclair', 'christmas', 'pretzel','other']
+    bakery_offer = []
 
     def __init__(self, name, kind, taste, additives, filling):
             self.name = name
@@ -7,3 +8,12 @@ class Cake:
             self.taste = taste
             self.additives = additives
             self.filling = filling
+    
+def create_bake():
+    nb_name = input("Name of the new bake: ")
+    nb_kind = input("Kind: ")
+    nb_taste = input("Taste: ")
+    nb_additives = input("Additives: ")
+    nb_filling = input("Filling: ")
+    new_bake = Cake(nb_name, nb_kind, nb_taste, nb_additives, nb_filling)
+    print(new_bake.name)
