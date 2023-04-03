9902d01b62f5c7da058bf1f5bf9e33a106bb87e1
Patrycja Kopinska <maliszewska.p@gmail.com>
2023-04-03 17:40:10 +0200

    adding the ability to create a new cake

--- The_Rolling_Scones_bakery.py
+++ The_Rolling_Scones_bakery.py
@@ -1,3 +1,10 @@
+import TRS_cake as cake
+#TO DO: creating a list of cakes, creating variables for the entered cakes and adding them to the list
+#import globals
+#   for i in range(100):
+#       globals()[f"cake_{i+1}"] = cake.Cake(nb_name, nb_kind, nb_taste, nb_additives, nb_filling)
+
+
 print("""Choose a profile:
 1. Bakery worker
 2. Customer""")
@@ -21,8 +28,24 @@ if user_profile == "Bakery worker":
     1. Create a new bake
     2. Change an existing bake
     """)
+    action = ""
+    while action != "1" or action != "2":
+        print("Select 1 or 2:")
+        action = input()
+        if action == "1":
+            action = "Create bake"
+            print("Create a new bake")
+            break
+        if action == "2":
+            action = "Change bake"
+            print("Change bake")
+            break
+
 if user_profile == "Customer":
     print("""Choose what you want to do:
     1. Submit your order
     2. Browse the offer
-    3. Browse favorites""")    
+    3. Browse favorites""")
+
+if action == "Create bake":
+    cake.create_bake()
