ff3d7ce912a406e2dc3426963be9dfd4e22c2e36
Patrycja Kopinska <maliszewska.p@gmail.com>
2023-04-04 10:11:36 +0200

    changing the create_bake function and adding the main loop

--- TRS_cake.py
+++ TRS_cake.py
@@ -1,6 +1,5 @@
 class Cake:
     known_types = ['cake', 'muffin', 'meringue', 'biscuit', 'eclair', 'christmas', 'pretzel','other']
-    bakery_offer = []
 
     def __init__(self, name, kind, taste, additives, filling):
             self.name = name
@@ -16,4 +15,19 @@ def create_bake():
     nb_additives = input("Additives: ")
     nb_filling = input("Filling: ")
     new_bake = Cake(nb_name, nb_kind, nb_taste, nb_additives, nb_filling)
-    print(new_bake.name)
+    print("New bake:")
+    print(f"""
+    Name: {new_bake.name}
+    Kind: {new_bake.kind}
+    Taste: {new_bake.taste}
+    Additives: {new_bake.additives}
+    Filling: {new_bake.filling}
+    Do you want to save the bake? (Y/N)""")
+    answer = input("")
+    if answer.upper() == "Y":
+         save_bake(new_bake)
+
+    
+def save_bake():
+    print("Saving the bake....")
+    #TO DO: Create a file with the bakery offer and write a function that saves new cakes there
--- The_Rolling_Scones_bakery.py
+++ The_Rolling_Scones_bakery.py
@@ -1,10 +1,7 @@
 import TRS_cake as cake
-#TO DO: creating a list of cakes, creating variables for the entered cakes and adding them to the list
-#import globals
-#   for i in range(100):
-#       globals()[f"cake_{i+1}"] = cake.Cake(nb_name, nb_kind, nb_taste, nb_additives, nb_filling)
 
 
+#---    user profile selection  ---
 print("""Choose a profile:
 1. Bakery worker
 2. Customer""")
@@ -23,29 +20,38 @@ while user_profile != "1" or user_profile != "2":
         print("Customer profile")
         break
 
-if user_profile == "Bakery worker":
-    print("""Choose what you want to do:
-    1. Create a new bake
-    2. Change an existing bake
-    """)
-    action = ""
-    while action != "1" or action != "2":
-        print("Select 1 or 2:")
-        action = input()
-        if action == "1":
-            action = "Create bake"
-            print("Create a new bake")
-            break
-        if action == "2":
-            action = "Change bake"
-            print("Change bake")
-            break
+#---    main loop   ---
 
-if user_profile == "Customer":
-    print("""Choose what you want to do:
-    1. Submit your order
-    2. Browse the offer
-    3. Browse favorites""")
+while True:
 
-if action == "Create bake":
-    cake.create_bake()
+    if user_profile == "Bakery worker":
+        print("""Choose what you want to do:
+        1. Create a new bake
+        2. Change an existing bake
+        """)
+        action = ""
+        while action != "1" or action != "2":
+            print("Select 1 or 2:")
+            action = input()
+            if action == "1":
+                action = "Create bake"
+                print("Create a new bake")
+                break
+            if action == "2":
+                action = "Change bake"
+                print("Change bake")
+                break
+
+    if user_profile == "Customer":
+        print("""Choose what you want to do:
+        1. Submit your order
+        2. Browse the offer
+        3. Browse favorites""")
+
+    if action == "Create bake":
+        cake.create_bake()
+
+    #ask the user to continue
+    print("Do you want to terminate the program? (Y/N)")
+    if input("").upper().startswith("Y"):
+        break
